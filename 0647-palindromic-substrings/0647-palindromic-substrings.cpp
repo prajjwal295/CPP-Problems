43378f0b2c1fd9f3cb340761cc803be0af8d1c7a
Prajjwal Mishra <mishraprajjwal295@gmail.com>
2023-09-26 22:51:47 +0530

    Time: 167 ms (17.19%), Space: 6.5 MB (70.82%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -11,38 +11,8 @@ public:
         return true;
     }
 
-    void solve(int& cnt ,int idx , string s , vector<string> &temp ,  vector<vector<string>> &ans)
-    {
-        if(idx == s.size())
-        {
-            // for(int it=0;it<temp.size();it++)
-            // {
-            //     cout<<temp[it]<<"";
-            // }
-            // cout<<endl;
-            ans.push_back(temp);
-            return ;
-        }
-
-        for(int i=idx;i<s.size();i++)
-        {
-            if(isPallindrome(s,idx,i))
-            {
-                temp.push_back(s.substr(idx,i+1));
-                solve(cnt,i+1 , s, temp,ans);
-                temp.pop_back();
-            }
-        }
-
-            
-       
-    }
     int countSubstrings(string s) {
         int cnt = 0;
-        // int idx=0;
-        // vector<vector <string> >ans;
-        // vector<string>temp;
-        // solve(cnt,idx , s, temp , ans);
 
         for(int i=0;i<s.size();i++)
         {
