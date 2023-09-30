1f5e11dd1faf5cff4410151b4a48247624c5a7ca
Prajjwal Mishra <mishraprajjwal295@gmail.com>
2023-09-30 23:49:32 +0530

    Time: 20 ms (61.33%), Space: 11.4 MB (42.62%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        
+        string ans;
+        for(int i=0;i<s.size();i++)
+        {
+            if(ans.size() == 0)
+            {
+                ans.push_back(s[i]);
+            }
+            else{
+
+           
+               if(ans[ans.size()-1] == s[i])
+               {
+                   ans.pop_back();
+               }
+               else{
+                   ans.push_back(s[i]);
+               }
+                               
+            }
+           
+        }
+
+        return ans;
+    }
+};
