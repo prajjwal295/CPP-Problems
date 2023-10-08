08730b0ecf376d911e9855025d569d0db1f30009
Prajjwal Mishra <mishraprajjwal295@gmail.com>
2023-10-08 13:17:50 +0530

    Time: 4 ms (19.88%), Space: 6.6 MB (12.92%) - LeetHub

---
+++ 2284-smallest-value-of-the-rearranged-number/2284-smallest-value-of-the-rearranged-number.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    static bool cmp(char a , char b)
+    {
+        return a < b;
+    }
+
+    long long smallestNumber(long long num) {
+        
+        long long ans=0;
+        vector<char> temp;
+        int sign = 1;
+        string nums=to_string(num);
+        if(nums[0] == '-') sign = -1;
+        else sign = 1;
+
+        if(sign == 1)
+        temp.push_back(nums[0]);
+
+        for( int i=1;i<nums.size();i++ )
+        {
+            temp.push_back(nums[i]);
+        }
+
+        sort(temp.begin(),temp.end() , cmp);
+
+        if(sign == 1)
+        {
+            int i=0;
+            while(true && i<temp.size()-1)
+            {
+                if(temp[i] == '0')
+                i++;
+                else
+                break;
+            }
+            
+            swap(temp[i],temp[0]);
+
+                for(int i=0.;i<temp.size();i++)
+        {
+          cout<<temp[i]<<endl;
+        }
+
+            for(int i=0;i<temp.size();i++)
+            {
+                ans = ans*10 + (temp[i]-'0');
+            }
+        }
+        else{
+             for(int i=temp.size()-1;i>=0;i--)
+            {
+                ans = ans*10 + (temp[i]-'0');
+            }
+
+            ans*=-1;
+        }
+
+        return ans;
+    }
+};
