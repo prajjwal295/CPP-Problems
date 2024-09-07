97046b5088f0d427a9d866b6a294ebf22fd9eb65
Prajjwal Mishra <mishraprajjwal295@gmail.com>
2024-09-07 15:31:22 +0530

    Time: 4 ms (57.25%) | Memory: 10.5 MB (40.62%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string reverseWords(string input) {
+        vector<string>ans;
+
+    string temp = "";
+
+    for(auto it:input)
+    {
+        if(it == ' ')
+        {
+            if(temp.size()>0){
+            ans.push_back(temp);
+            cout<<temp<<endl;
+        }
+            temp = "";
+        }
+        else{
+            temp+=it;
+        }
+    }
+ if(temp.size()>0){
+            ans.push_back(temp);
+            cout<<temp<<endl;
+        }
+        
+
+    reverse(ans.begin(),ans.end());
+
+    string res = "";
+
+    for(int i=0;i<ans.size();i++)
+    {
+        if(ans[i] != " " || ans[i] == "")
+        {
+            res+=ans[i];
+            if(i!=ans.size()-1)
+            {
+                res+=' ';
+            }
+        }
+        
+    }
+
+    return res;
+    }
+};
