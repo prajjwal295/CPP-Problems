cb49e72d8b95066bbd7bc872ac6cf69455f4957d
Prajjwal Mishra <mishraprajjwal295@gmail.com>
2025-03-08 19:11:37 +0530

    Time: 18 ms (3.76%) | Memory: 9.2 MB (5.10%) - LeetSync

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        
+        vector<int>mp(blocks.size());
+
+        for(int i=0;i<blocks.size();i++)
+        {
+            int n = 0;
+            char ch = blocks[i];
+            if(ch == 'B')
+            n = 1;
+            
+            if(i==0)
+            {
+                mp[i] = n;
+            }
+            else{
+                mp[i] = mp[i-1] + n;
+            }
+        }
+
+        for(auto it:mp)
+        {
+            cout<<it<<" ";
+        }
+        int ans = INT_MAX;
+        int temp = 0;
+        for(int i=0;i<mp.size()-k+1;i++)
+        {
+            int s;
+            if(i == 0)
+            s=0;
+            else
+            s = mp[i-1];
+            int e = mp[i+k-1];
+
+            int sub = e-s;
+            temp = k-sub;
+            ans = min(temp,ans);
+        }
+
+        return ans;
+    }
+};
